b37377ba9982a884d9bcabea1a0cb8b32d174231
Jurijs Jeskins <jurijs.jeshkins@gmail.com>
2013-07-05 21:14:00 +0300

    changes in web.js for index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello Jura!');
+    var buffer = fs.readFileSync("index.html");
+    
+    response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
