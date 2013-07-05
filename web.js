dbb7229374feb6402f5aa33380dcb359b0af0511
Jurijs Jeskins <jurijs.jeshkins@gmail.com>
2013-07-05 21:15:25 +0300

    bug with buffer-toString

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
     var buffer = fs.readFileSync("index.html");
     
-    response.send(buffer);
+    response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
